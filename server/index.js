daf3a8360981837b92d622f7e2f55ceb66dbc1bb
imdoug <imdoug@icloud.com>
2023-03-23 12:16:35 -0400

    fix

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongodb/connect';
-import postRoutes from './routes/postRoutes';
-import dalleRoutes from './routes/dalleRoutes';
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
