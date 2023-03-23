c4d17e17fafdc3533ad108bb370184b6779bc812
imdoug <imdoug@icloud.com>
2023-03-23 12:38:17 -0400

    done

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-dotenv.config();
+dotenv.config(); 
 
 const app = express();
 app.use(cors());
